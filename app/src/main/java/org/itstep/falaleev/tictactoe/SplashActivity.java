25a0b637412777544291706a4145bca630c66399
Gordon Freeman <toxa82@gmail.com>
2015-07-25 18:26:43 +0300

    Add splash screen

---
+++ app/src/main/java/org/itstep/falaleev/tictactoe/SplashActivity.java
@@ -0,0 +1,29 @@
+package org.itstep.falaleev.tictactoe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 800;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
